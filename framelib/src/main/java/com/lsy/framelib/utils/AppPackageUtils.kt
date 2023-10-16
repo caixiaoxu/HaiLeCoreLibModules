118e937c5b0713b689fd3c7312e6e718e27c4972
Lsy <1228357237@qq.com>
2023-10-16 15:31:49 +0800

    跳转地图完成

--- framelib/src/main/java/com/lsy/framelib/utils/AppPackageUtils.kt
+++ framelib/src/main/java/com/lsy/framelib/utils/AppPackageUtils.kt
@@ -30,6 +30,7 @@ object AppPackageUtils {
         e.printStackTrace()
         ""
     }
+
     fun installApk(context: Context, apkFile: File) {
         val intent = Intent(Intent.ACTION_VIEW)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
@@ -44,6 +45,60 @@ object AppPackageUtils {
         } else {
             intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive")
         }
+        context.startActivity(intent)
+    }
+
+
+    /**
+     * 判断是否安装某个应用
+     */
+    fun checkAppInstalled(context: Context, pkgName: String?): Boolean {
+        if (pkgName == null || pkgName.isEmpty()) {
+            return false
+        }
+        val packageManager: PackageManager = context.packageManager
+        val info = packageManager.getInstalledPackages(0)
+        if (info.isNullOrEmpty()) {
+            return false
+        }
+        for (i in info.indices) {
+            if (pkgName == info[i].packageName) {
+                return true
+            }
+        }
+        return false
+    }
+
+    /**
+     * 通过包名 在应用商店打开应用
+     *
+     * @param packageName 包名
+     */
+    fun openApplicationMarket(context: Context, packageName: String) {
+        // 新版应用商店不再支付跳转
+//        try {
+//            val str = "market://details?id=$packageName"
+//            val localIntent = Intent(Intent.ACTION_VIEW)
+//            localIntent.data = Uri.parse(str)
+//            localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//            context.startActivity(localIntent)
+//        } catch (e: Exception) {
+//            // 打开应用商店失败 可能是手机没有安装应用市场
+//            e.printStackTrace()
+//            // 调用系统浏览器进入商城
+            val url = "http://app.mi.com/detail/163525?ref=search"
+            openLinkBySystem(context, url)
+//        }
+    }
+
+    /**
+     * 调用系统浏览器打开网页
+     *
+     * @param url 地址
+     */
+    fun openLinkBySystem(context: Context, url: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
         context.startActivity(intent)
     }
 }
